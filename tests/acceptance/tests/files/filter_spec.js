a101f45811d59043bf8a27f636d86822313b34d6
Felix Böhm <felixboehm@gmx.de>
2014-09-17 22:07:42 +0200

    fix filter timing

--- tests/acceptance/tests/files/filter_spec.js
+++ tests/acceptance/tests/files/filter_spec.js
@@ -18,7 +18,7 @@ var flow = protractor.promise.controlFlow();
 //================ FILTERS =============================================================//
 //======================================================================================//
 
-describe('Filter', function() {
+ddescribe('Filter', function() {
   var params = browser.params;
   var logninPage;
   var filesPage;
@@ -33,20 +33,20 @@ describe('Filter', function() {
     shareApi = new ShareApi(params.baseUrl);
   });
 
-  it('setup', function() {
+  iit('setup', function() {
     userPage.getAsUser(params.login.user, params.login.password, userPage.url);
     userPage.createNewUser('demo', 'password');
 
     filesPage.get();
 
     var create = function() {
-      filesPage.createFolder('sharedFoler');
+      filesPage.createFolder('sharedFolder');
       filesPage.createTxtFile('sharedFile');
       filesPage.createTxtFile('sharedFile2');
     };
 
     var share = function() {
-      shareApi.create('sharedFoler', 'demo', 0);
+      shareApi.create('sharedFolder', 'demo', 0);
       shareApi.create('sharedFile.txt', 'demo', 0);
       shareApi.create('sharedFile2.txt', 'demo', 0);
     };
@@ -54,7 +54,7 @@ describe('Filter', function() {
     flow.execute(create);
     flow.execute(share);
 
-    expect(filesPage.listFiles()).toContain('sharedFoler', 'sharedFile', 'sharedFile2');
+    expect(filesPage.listFiles()).toContain('sharedFolder', 'sharedFile', 'sharedFile2');
     loginPage.logout(); 
   });
 
@@ -64,27 +64,29 @@ describe('Filter', function() {
     browser.wait(function() {
       return filesPage.listFiles();
     });
-    expect(filesPage.listFiles()).toContain('sharedFoler', 'sharedFile', 'sharedFile2');
+    expect(filesPage.listFiles()).toContain('sharedFolder', 'sharedFile', 'sharedFile2');
     loginPage.logout();
   });
 
   it('should show files shared with others', function() {
     filesPage.getAsUser(params.login.user, params.login.password);
     filesPage.filterSharedWhithOthers.click();
-    expect(filesPage.listFiles()).toContain('sharedFoler', 'sharedFile', 'sharedFile2');
+    expect(filesPage.listFiles()).toContain('sharedFolder', 'sharedFile', 'sharedFile2');
   });
 
   it('should show all files', function() {
     filesPage.getAsUser(params.login.user, params.login.password);
     filesPage.filterAllFiles.click();
-    expect(filesPage.listFiles()).toContain('music', 'documents', 'photos', 'sharedFoler', 'sharedFile', 'sharedFile2');
+    expect(filesPage.listFiles()).toContain('sharedFolder', 'sharedFile', 'sharedFile2');
   });
 
-  it('clean up', function() {
+  iit('clean up', function() {
     filesPage.getAsUser(params.login.user, params.login.password).then(function() {
-      filesPage.deleteFile('sharedFile.txt');
-      filesPage.deleteFile('sharedFile2.txt');
-      filesPage.deleteFolder('sharedFoler');
+      filesPage.deleteFile('sharedFile.txt').then(function(){
+        filesPage.deleteFile('sharedFile2.txt').then(function(){
+          filesPage.deleteFolder('sharedFolder');          
+        });
+      });
     });
   });
 });
